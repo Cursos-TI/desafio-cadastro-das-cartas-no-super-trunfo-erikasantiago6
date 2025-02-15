55d9a6fd77b09bfca808927c54b3fe739e106843
erikasantiago6 <erikasantiagoab3085@gmail.com>
2025-02-15 00:05:56 +0000

    criei super_trunfo

---
+++ SUper_TRUnfo/super_trunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("super trunfo \n");
+
+    printf("digite seu estado: \n");
+    scanf("%s, &estado");
+
+    printf("digite o codigo da carta: \n");
+    scanf("%d, &codigo da carta");
+   
+    printf("digite o nome da cidade: \n");
+    scanf("%s, &nome da cidade");
+
+    printf("digite o numero da população: \n");
+    scanf("%d, &população");
+
+    printf("digite a área em km: \n");
+    scanf("%f, &área em km");
+
+    printf("digite o PIB: \n");
+    scanf("%f, &PIB");
+
+    printf("digite o numero de pontos turistocos: \n");
+    scanf("%d, &numero de pontos turisticos");
+
+    
+return 0;
+}
